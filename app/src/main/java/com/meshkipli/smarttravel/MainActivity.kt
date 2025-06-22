4819d018553325a0f5c133a12bfdeb4a1652a2a1
Meshkiplii <sangmeshack9@gmail.com>
2025-06-22 15:22:49 +0300

    Modify MainActivity to launch OnBoardingActivity
    The MainActivity now directly starts the OnBoardingActivity using an Intent and then finishes itself.
    Removed the default Jetpack Compose UI elements.

--- app/src/main/java/com/meshkipli/smarttravel/MainActivity.kt
+++ app/src/main/java/com/meshkipli/smarttravel/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.meshkipli.smarttravel
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen // Import this
-import com.meshkipli.smarttravel.ui.theme.SmartTravelTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,36 +13,9 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
-        // Optional: Keep the splash screen visible for longer if needed (e.g., for data loading)
-        // splashScreen.setKeepOnScreenCondition { /* your condition, e.g., !viewModel.isDataLoaded */ true }
-        // If you use setKeepOnScreenCondition, make sure it eventually becomes false to dismiss the splash screen.
-
-        setContent {
-            SmartTravelTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        val intent = Intent(this, OnBoardingActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartTravelTheme {
-        Greeting("Android")
-    }
-}
