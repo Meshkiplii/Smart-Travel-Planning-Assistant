d22bd6f2d3e0ee5ab89dd29df3e8b49b201a1313
Meshkiplii <sangmeshack9@gmail.com>
2025-06-29 18:13:35 +0300

    create TripDetailsActivity

---
+++ app/src/main/java/com/meshkipli/smarttravel/TripDetailsActivity.kt
@@ -0,0 +1,22 @@
+package com.meshkipli.smarttravel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.meshkipli.smarttravel.screens.TripDetailsScreen
+import com.meshkipli.smarttravel.ui.theme.SmartTravelTheme
+
+class TripDetailsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SmartTravelTheme {
+                TripDetailsScreen(
+                    onNavigateBack = {
+                        finish()
+                    }
+                )
+            }
+        }
+    }
+}
